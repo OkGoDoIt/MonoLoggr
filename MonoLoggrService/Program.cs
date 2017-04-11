e5b373d5259b89e0b16b9918d85a11e3566a5fe7
Roger <roger@pincombe.com>
2017-04-11 00:25:36 -0700

    image uploading and tagging

---
+++ MonoLoggrService/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Vision;
+using System.Configuration;
+using Microsoft.ProjectOxford.Vision.Contract;
+using System.Diagnostics;
+
+namespace MonoLoggrService
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			string loadDir = args.Length == 1 ? args[0] : keys.Default.local_load_dir;
+			Console.WriteLine($"Loading images from {loadDir}...");
+
+			var files = Directory.GetFiles(loadDir);
+
+			Console.WriteLine($"Found {files.Length} images");
+
+
+
+
+
+			var t = AnalyzePhotos(files);
+			t.Wait();
+
+			Console.WriteLine("Done");
+
+		}
+
+		static async Task AnalyzeSinglePhoto(string file)
+		{
+			VisionServiceClient VisionServiceClient = new VisionServiceClient(keys.Default.ms_cog_serv_api_key);
+
+			using (Stream imageFileStream = File.OpenRead(file))
+			{
+				//
+				// Analyze the image for all visual features
+				//
+				Debug.WriteLine($"Calling VisionServiceClient.AnalyzeImageAsync({file})...");
+				VisualFeature[] visualFeatures = new VisualFeature[] { VisualFeature.Adult, VisualFeature.Categories, VisualFeature.Color, VisualFeature.Description, VisualFeature.Faces, VisualFeature.ImageType, VisualFeature.Tags };
+				AnalysisResult analysisResult = await VisionServiceClient.AnalyzeImageAsync(imageFileStream, visualFeatures);
+
+				string resultTxt = (analysisResult.Categories.Where(cat => cat.Score > 0.1).FirstOrDefault()?.Name ?? "(no cat)")
+					+ ", " + string.Join(", ", analysisResult.Tags.Where(tag => tag.Confidence > 0.1).Take(3).Select(tag => tag.Name))
+					+ ": " + analysisResult.Description.Captions.FirstOrDefault()?.Text ?? "(no description)";
+
+				if (analysisResult.Description.Captions.Max(cap => cap.Confidence) > 0.1)
+					Console.WriteLine(resultTxt);
+
+				Debug.WriteLine($"Task completed for {file}");
+			}
+		}
+
+
+		static async Task AnalyzePhotos(IEnumerable<string> filenames)
+		{
+			List<Task> tasks = new List<Task>();
+
+			foreach (var file in filenames)
+			{
+				Task t = Task.Run(() => AnalyzeSinglePhoto(file));
+				tasks.Add(t);
+				
+				Debug.WriteLine($"Task added for {file}");
+
+				await Task.Delay(500);
+			}
+
+			await Task.WhenAll(tasks);
+
+			foreach (var ex in tasks.Where(task => task.IsFaulted).Select(task => task.Exception))
+			{
+				Console.WriteLine(ex.ToString());
+			}
+			
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # MonoLoggr
+Lifelogging monitor: search everything you've ever said or seen so you never forget anything.  It could help you find "that guy I talked with at the ML conference last November about a wearable idea" or "the taco truck I saw in Denver last month."  
+
+We can search our texts and emails to remind ourselves of previous conversations, but the things we say and see are lost forever. Not anymore! Monologgr keeps track of everything you experience and makes it easily searchable.
+
+"What company did that guy from board game night say he worked at?"
+"Who did I meet at that AI hackathon last month that was working on lifelogging?"
+"Where is the pet shop where I saw that really cute corgi yesterday?"
+"How many times have I eaten ice cream since I started my diet in January?"
+
+It will take photos of what you see using a Memento Clip lifelogging camera and record everything you hear using Glass or an Apple Airpod, then analyzes it using off-the-shelf machine learning API's for speech recognition and image recognition.  Also could use location tracking (using Moves) and other lifelogging data sources (life fitbit for heart rate) or maybe I can press as button on my smartwatch to save a moment in time as important so I'll remember to review later.
+
+Most of these data sources are already built, most of the hard ML work has already been done.  The hardware mostly exists although it's currently pretty rough.  Basically someone needs to tie everything together and make it simple to search, easy to use, and clearly useful.
+
+My prototype this weekend will use google glass or an apple airpod for audio recording and a memento clip lifelogging camera, but these are clearly just for prototyping, not long term.  I just got back from a trip to Shenzhen where I met a couple hardware companies who make knockoff Memento Clip camera which are willing to customize it to have the necessary functionality.
+
+I made a first attempt at building a simplified version of this (audio only) at the TechCrunch Disrupt SF hackathon in fall 2015: [https://devpost.com/software/monologgr](https://devpost.com/software/monologgr)
+
+![Monologgr logo](https://dl.dropboxusercontent.com/u/1302719/monologgr/logo.png?c=1)
+
+
+Created by Roger Pincombe
